ea5e6902ecc5837826c12efb35edaa722c917e97
rafa30052003 <rafalucenavalle9@gmail.com>
2023-03-09 19:15:08 +0100

    interfaces repo

--- src/main/java/Grupo3/GestorCompeticiones/interfaces/repo/iRepoPruebas.java
+++ src/main/java/Grupo3/GestorCompeticiones/interfaces/repo/iRepoPruebas.java
@@ -1,5 +1,17 @@
 package Grupo3.GestorCompeticiones.interfaces.repo;
 
-public interface iRepoPruebas {
+import Grupo3.GestorCompeticiones.model.DO.Aparato;
+import Grupo3.GestorCompeticiones.model.DO.Categoria;
+import Grupo3.GestorCompeticiones.model.DO.Prueba;
+import Grupo3.GestorCompeticiones.model.DO.TipoPrueba;
 
+public interface iRepoPruebas {
+	
+	String muestraPrueba();
+	
+	boolean insertaPrueba(Prueba p);
+	
+	Prueba buscaPrueba(TipoPrueba prueba, Categoria categoria, Aparato aparato);
+	
+	boolean eliminaPrueba(Prueba p);
 }

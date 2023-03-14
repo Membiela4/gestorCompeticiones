9e1bca542885f2063f15399f6ed2b2cfe86c31b2
rafa30052003 <rafalucenavalle9@gmail.com>
2023-03-14 09:23:53 +0100

    actualizaciones

---
+++ src/main/java/Grupo3/GestorCompeticiones/ejecutable/Main.java
@@ -0,0 +1,13 @@
+package Grupo3.GestorCompeticiones.ejecutable;
+
+import Grupo3.GestorCompeticiones.controlador.ControladorPrincipal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ControladorPrincipal  p = new ControladorPrincipal();
+		p.controlarMenuPrincipal();
+
+	}
+
+}
